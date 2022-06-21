b7ba1181e86ddb0731af2782ced50982eeafc190
Sven Balnojan <sven.balnojan@mercateo.com>
2022-06-21 09:59:22 +0200

    add truly broken unit test

---
+++ dbt_stuff/dbt/dbt_stuff/tests/test_1.sql
@@ -0,0 +1,25 @@
+{{
+    config(
+        tags=['unit-test']
+    )
+}}
+
+{% call dbt_unit_testing.test ('1_report','has new column') %}
+
+  {% call dbt_unit_testing.mock_ref ('source_customers') %}
+    select 1 as customer_id
+  {% endcall %}
+
+  {% call dbt_unit_testing.mock_ref ('source_mkt') %}
+    select 1 as duh
+  {% endcall %}
+
+  {% call dbt_unit_testing.mock_ref ('source_orders') %}
+    select 1 as duh
+  {% endcall %}
+
+  {% call dbt_unit_testing.expect() %}
+    select  new_column
+  {% endcall %}
+
+{% endcall %}
